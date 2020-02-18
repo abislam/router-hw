acfe6d13814c48197ad81349be162c1f3ff7a0fb
Abed <aislam360@gmail.com>
2020-02-17 22:36:51 -0500

    Initial commit. Three boxes created

---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div>
+            <div style={{backgroundColor: "red", height: 100, width: 100}}>Box1</div>
+            <div style={{backgroundColor: "green", height: 100, width: 100}}>Box2</div>
+            <div style={{backgroundColor: "blue", height: 100, width: 100}}>Box3</div>
+        </div>
+    )
+}
+
+export default Home;
